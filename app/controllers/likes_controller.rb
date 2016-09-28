fbc05037b535e93dbc74e800558c87c94f2ebe7c
Marco Molina <marcomolina11@gmail.com>
2016-09-28 18:22:07 -0400

    Added User authentication for creating and deleting likes, along with the corresponding tests.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+  before_action :require_login
   def create
   	user = User.find(session[:user_id])
   	secret = Secret.find(params[:secret_id])
--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-# RSpec.describe LikesController, type: :controller do
-
-#   describe "GET #create" do
-#     it "returns http success" do
-#       get :create
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-#   describe "GET #destroy" do
-#     it "returns http success" do
-#       get :destroy
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-# end
+RSpec.describe LikesController, type: :controller do
+  before do
+    @user = create_user
+    @secret = @user.secrets.create(content: 'test secret')
+  end
+  describe "when not logged in" do
+    before do
+      session[:user_id] = nil
+   	end
+   	it "cannot like a secret" do 
+   	  post :create, secret_id: @secret 
+   	  expect(response).to redirect_to('/sessions/new')
+   	end
+   	it "cannot unlike a secret" do 
+   	  delete :destroy, secret_id: @secret
+   	  expect(response).to redirect_to('/sessions/new')
+   	end
+  end
+end
